b69b7f5e20f6e98765f1d423c4b3bea37d9b481f
Franklin <frankkiogora@yahoo.com>
2018-08-29 17:08:27 -0700

    Refactored code

--- is_anagram.py
+++ is_anagram.py
@@ -7,28 +7,30 @@ Write a program to determine if two given strings are anagram of each other.
 s1 = "fairy tales "
 s2 = "rail safety "
 
-s1 = s1.replace(" ", "").lower()
-s2 = s2.replace(" ", "").lower()
+s3 = "Kirimi"
+s4 = "Karimi"
 
-def is_anagram(s1, s2):
-    ht = dict()
+
+def is_anagram(s1,s2):
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+
     if len(s1) != len(s2):
         return False
-    for i in s1:
-        if i in ht:
-            ht[i] += 1
-        else:
-            ht[i] = 1
 
-    for i in s2:
-        if i in ht:
-            ht[i] -= 1
-        else:
-            ht[i] = 1
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    for i in ht:
-        if ht[i] != 0:
-            return False
-    return True
+
+    s1 = s1.lower()
+    s2 = s2.lower()
+
+    dick_1 = dict.fromkeys(list(alphabet), 0)
+
+    dick_2 = dict.fromkeys(list(alphabet), 0)
+
+    for i in range(len(s1)):
+        dick_1[s1[i]] +=1
+        dick_2[s2[i]] +=1
+    return dick_1 == dick_2
 
 print is_anagram(s1, s2)
